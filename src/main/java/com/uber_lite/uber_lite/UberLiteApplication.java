13554548ca907328e8744b4874f5eccd6de1f772
Nikhila Pulloju <nikhilapulloju@Nikhilas-MacBook-Air.local>
2025-10-01 00:09:21 +0530

    DB:1

--- src/main/java/com/uber_lite/uber_lite/UberLiteApplication.java
+++ src/main/java/com/uber_lite/uber_lite/UberLiteApplication.java
@@ -1,13 +1,34 @@
 package com.uber_lite.uber_lite;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.uber_lite.uber_lite.domain.Role;
+import com.uber_lite.uber_lite.domain.User;
+import com.uber_lite.uber_lite.repo.UserRepository;
 
 @SpringBootApplication
 public class UberLiteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UberLiteApplication.class, args);
+
 	}
+
+	@Bean
+CommandLineRunner seed(UserRepository users) {
+    return args -> {
+        if (users.count() == 0) {
+            users.save(User.builder()
+                    .name("Test Rider")
+                    .phoneNumber("9999999999")
+                    .role(Role.RIDER)
+                    .build());
+        }
+        System.out.println("Users in DB: " + users.count());
+    };
+}
 
 }
--- src/main/java/com/uber_lite/uber_lite/domain/User.java
+++ src/main/java/com/uber_lite/uber_lite/domain/User.java
@@ -2,6 +2,7 @@ package com.uber_lite.uber_lite.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -23,7 +24,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @@Column(nullable = false)
+    @Column(nullable = false)
     private String name;
 
     @Column(nullable = false, unique = true)
