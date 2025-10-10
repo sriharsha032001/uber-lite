ece9acb6fdb07ddafd316920936a4663f93e73a7
Nikhila Pulloju <nikhilapulloju@Nikhilas-MacBook-Air.local>
2025-10-10 08:38:23 +0530

    Driver Entity

--- src/main/java/com/uber_lite/uber_lite/domain/Driver.java
+++ src/main/java/com/uber_lite/uber_lite/domain/Driver.java
@@ -1,10 +1,17 @@
 package com.uber_lite.uber_lite.domain;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,8 +29,20 @@ public class Driver {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @OneToOne(optional = false)
+    @MapsId
+    @JoinColumn(name = "id")
+    private User user;
+     
+    @Column(nullable = false)
+    private String licenseNumber;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private DriverStatus status;
 
+    @OneToOne(optional = false, fetch = FetchType.EAGER)
+    @JoinColumn(name = "vehicle_id")
+    private Vehicle vehicle;
 
-    
 }
