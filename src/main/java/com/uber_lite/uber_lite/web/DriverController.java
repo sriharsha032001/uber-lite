35e3c4011bc3cf2da97e9b17ef09ed12f713fcd7
Harsha <ramchandrasriharsha@gmail.com>
2025-10-24 22:18:11 +0530

    Driver Controller

---
+++ src/main/java/com/uber_lite/uber_lite/web/DriverController.java
@@ -0,0 +1,13 @@
+package com.uber_lite.uber_lite.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/drivers")
+@RequiredArgsConstructor
+public class DriverController { 
+    
+}
