d1e1e7f29d72b8bb7bea518412acf179f5dd9077
Harsha <ramchandrasriharsha@gmail.com>
2025-10-31 19:17:49 +0530

    Ride matching strategy

--- src/main/java/com/uber_lite/uber_lite/core/matching/DriverMatchingStrategy.java
+++ src/main/java/com/uber_lite/uber_lite/core/matching/DriverMatchingStrategy.java
@@ -5,6 +5,6 @@ import java.util.Optional;
 import com.uber_lite.uber_lite.domain.Driver;
 
 public interface DriverMatchingStrategy {
-    Optional<Driver> match(double PickupLat, double PickupLon, String type);
+    Optional<Driver> match(double PickupLat, double PickupLon, String Vehicle);
     
 }
